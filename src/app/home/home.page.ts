7ed6c96c437b70cf59c149f926c9f722e3267667
jspe2bb26 <carreonromer@gmail.com>
2025-03-14 15:40:55 +0800

    Location Tracking
    - Added getCurrentLocation() to get current location

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,29 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  location: any = {};
 
-  constructor() {}
 
+  constructor() {
+    this.getCurrentLocation();
+  }
+
+  async getCurrentLocation() {
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      const { latitude, longitude } = coordinates.coords;
+      this.location.latitude = latitude;
+      this.location.longitude = longitude;
+    } catch(error) {
+      alert("Unable to get current location as of this moment: "+error);
+    }
+  }
+
+  async getCurrentWeatherReport() {
+    try {
+      
+    } catch(error) {
+      alert("Unable to get current weather report as of this moment: "+error);
+    }
+  }
 }
